f7c832dbed5b7f849e4df49b072413f0262918b9
loriyang9 <loriyang9@gmail.com>
2026-05-24 13:06:06 +0800

    Fix CORS: allow Access-Control-Allow-Credentials: true

--- server/index.ts
+++ server/index.ts
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
   }
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
